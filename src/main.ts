9656290867495bd8697376a11e4c623d9a1c630c
Luke Chigges <git+git@ctrlaltdelight.org>
2023-09-12 13:56:50 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// Add
+export function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+// Subtract
+export function subtract(num1: number, num2: number): number {
+    return num1 - num2;
+}
+
+// Multiply
+export function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Divide
+export function divide(num1: number, num2: number): number {
+    return num1 / num2;
+}
